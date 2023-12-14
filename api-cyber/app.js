0f896fa648ed8a947aec3d490af2f109103e6adb
raphou <raphael.coussement@diiage.org>
2023-12-14 09:11:24 +0100

    apiCyber

---
+++ api-cyber/app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors'); // permet d'activer CORS
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+app.use(cors());
+// Middleware pour activer CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
+require('dotenv').config(); // permet de lire le fichier .env et de le mettre dans process.env
+console.log(process.env);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
