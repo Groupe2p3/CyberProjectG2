7f5f75bfd5a721aae9685380ebb53be2c3ee41f8
raphou <raphael.coussement@diiage.org>
2023-12-21 13:08:50 +0100

    pageAccueilWithLinks

--- react-g2/src/components/Navbar.jsx
+++ react-g2/src/components/Navbar.jsx
@@ -1,14 +1,17 @@
-// Navbar.js
 import React from 'react';
 import logoImage from '../assets/montain.png';
+import logoProfile from '../assets/profile-user.png';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <div className="navbar-logo"><img src={logoImage} alt="logo" className="logo-image"/></div>
+        <div className="navbar-logo">
+          <a href="/"><img src={logoImage} alt="logo" className="logo-image" /></a>
+        </div>
         <ul className="navbar-menu">
-          <li className="navbar-item">Mon profil
+          <li className="navbar-item">
+            <a href="/profile" className='profile'><img src={logoProfile} alt="logo" className="logo-profile" /></a>
           </li>
         </ul>
       </div>
--- react-g2/src/main.jsx
+++ react-g2/src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Profil from './components/Profil'
+import ErrorPage from "./error-page";
+import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/profile",
+    element: <Profil />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
